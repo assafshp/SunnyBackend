22a1987842899d9cda3f233abd0e7bcb12bd3c78
yarink <yarink@liveperson.com>
2018-06-11 21:54:24 +0300

    refactoring modules

--- resources_bootstrap/index.js
+++ resources_bootstrap/index.js
@@ -1,40 +1,13 @@
-/**
- * Copyright 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 'use strict';
 
 // [START imports]
 var firebase = require('firebase-admin');
 // [END imports]
-var schedule = require('node-schedule');
 var Promise = require('promise');
-var escape = require('escape-html');
 
-// TODO(DEVELOPER): Configure your email transport.
-// Configure the email transport using the default SMTP transport and a GMail account.
-// See: https://nodemailer.com/
-// For other types of transports (Amazon SES, Sendgrid...) see https://nodemailer.com/2-0-0-beta/setup-transporter/
 
-var fs = require('fs')
-/*var expandHomeDir = require('expand-home-dir')//help to read from home path in linux filesystem
 
-var serviceAccount ;//= require('./../firebase.json');
-const Storage = require('@google-cloud/storage');
-//var gcloud = require('@google-cloud');
-var gcs ;//
-const CLOUD_BUCKET = 'friendlypix-d292b.appspot.com';//config.get('CLOUD_BUCKET');*/
 var bucket;//
 var profileImagesLocalPath = 'upload-resources/sun_profile_images/';
 var profileImagesRemotePath = 'profile_images/';
@@ -98,17 +71,6 @@ var uploadProfileImagesFull =async function(index) {
 
 }
 
-/*
-var options = {
-    destination: 'new-image.png',
-    resumable: true,
-    validation: 'crc32c',
-    metadata: {
-        metadata: {
-            event: 'Fall trip to the zoo'
-        }
-    }
-};*/
 
 var uploadProfileImagesThumb =  function(object) {
     var uploadFiles = new Promise(function(resolve, reject){
--- sunny_node_backend/backend/index.js
+++ sunny_node_backend/backend/index.js
@@ -1,87 +1,71 @@
-/**
- * Copyright 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 'use strict';
 
-// [START imports]
 var firebase = require('firebase-admin');
-// [END imports]
-var schedule = require('node-schedule');
+
 var Promise = require('promise');
 
 var cron = require('node-cron');
 
-
-
-
-// TODO(DEVELOPER): Configure your email transport.
-// Configure the email transport using the default SMTP transport and a GMail account.
-// See: https://nodemailer.com/
-// For other types of transports (Amazon SES, Sendgrid...) see https://nodemailer.com/2-0-0-beta/setup-transporter/
-
 var fs = require('fs')
-var expandHomeDir = require('expand-home-dir')//help to read from home path in linux filesystem
+var homePath = expandHomeDir('~/dev/private_keys/' + env + '/pk.json');
 
-var serviceAccount ;//= require('./../firebase.json');
 const Storage = require('@google-cloud/storage');
-//var gcloud = require('@google-cloud');
-var gcs ;//
-var bucket;//
-var app_id ='friendlypix-d292b';
-var CLOUD_BUCKET = app_id;//config.get('CLOUD_BUCKET');
+var gcs;
+var bucket;
+
+//tmp values
+var app_id = 'friendlypix-d292b';
+var CLOUD_BUCKET = app_id; //config.get('CLOUD_BUCKET');
 var env = 'stg';
-var profileImagesLocalPath = 'profile_images/';
 
+//Constants
+const bucket_suffix ='.appspot.com';
 
-var upload_resources_service = require('./upload-resources/index');
-// print process.argv
-/*process.argv.forEach(function (val, index, array) {
-    console.log(index + ': ' + val);
-});*/
-if(process.argv[2]!=null){
+//import upload_resources functionality
+var upload_resources_service = require('../../resources_bootstrap/index');
+
+//get external arguments for run configurations
+if (process.argv[2] != null) {
     env = process.argv[2];
 }
 
-if(process.argv[3]!=null){
+if (process.argv[3] != null) {
     app_id = process.argv[3];
-    CLOUD_BUCKET = app_id+'.appspot.com';
+    CLOUD_BUCKET = app_id + bucket_suffix;
 }
 
-//return;
-var homePath = expandHomeDir('~/dev/private_keys/'+env+'/pk.json');
+/**
+ * load service account content to pass for further authentication
+ * @returns {*|Promise}
+ */
 var buildServiceAccountPath = function() {
-    var promise = new Promise(function(resolve, reject){
-        fs.readFile(homePath, 'utf8', function (err, data) {
+    var promise = new Promise(function(resolve, reject) {
+        fs.readFile(homePath, 'utf8', function(err, data) {
             if (err) {
                 reject(console.log(err));
             }
 
             console.log("loaded cred json from file system")
-            resolve({data: data});
+            resolve({
+                data: data
+            });
         });
     });
     return promise;
 };
 
-
-var loadServiceAccountPath = function(firebaseCredResults) {
-    var promise = new Promise(function(resolve, reject){
+/**
+ * authenticate firebase client which will pass to consumer all over the service
+ * @param firebaseCredResults
+ * @returns {*|Promise}
+ */
+var loadServiceAccountFromPath = function(firebaseCredResults) {
+    var promise = new Promise(function(resolve, reject) {
         if (!firebase.apps.length) {
             firebase.initializeApp({
                 credential: firebase.credential.cert(JSON.parse(firebaseCredResults.data)),
-                databaseURL: 'https://'+app_id+'.firebaseio.com'
+                databaseURL: 'https://' + app_id + '.firebaseio.com'
             });
         }
 
@@ -89,68 +73,64 @@ var loadServiceAccountPath = function(firebaseCredResults) {
             projectId: app_id,
             keyFilename: homePath
         });
-         bucket = gcs.bucket(CLOUD_BUCKET);
-
+        bucket = gcs.bucket(CLOUD_BUCKET);
 
         //is fire loaded? if not stop the process
-        if(firebase.apps.length ) {
+        if (firebase.apps.length) {
             console.log("firebase loaded");
-            resolve("foo")
-        }
-        else {
+            resolve()
+        } else {
             console.log("failed to load firebase");
             reject(process.exit())
         }
-      /*  bucket.upload(profileImagesLocalPath+'foo.txt', function(err, file) {
-            if (err)
-                throw new Error(err);
-        });*/
+
     });
     return promise;
 };
 
-// Get a database reference to our posts
 
-
-
-
-var HOUR_IN_MILLI_SECONDS = 60 * 60 * 1000*1
+const HOUR_IN_MILLI_SECONDS = 60 * 60 * 1000 * 1 // seconds*minutes*milli*hours
+/**
+ *  Schedulers for mark last day posts as "should be deleted"
+ * @returns {*|Promise}
+ */
 var scheduleDeletionPosts = function() {
-    var promise = new Promise(function(resolve, reject){
+    var promise = new Promise(function(resolve, reject) {
         var db = firebase.database();
         var ref = db.ref("/posts_meta/").orderByChild("delete").equalTo(false);
 
-        cron.schedule('* * * * *', function(){
+        cron.schedule('0 * * * *', function() {
             console.log('running a schedule every hour');
 
-        ref.on("child_added", function(snapshot, prevChildKey) {
-            var postMeta = snapshot.val();
+            ref.on("child_added", function(snapshot, prevChildKey) {
+                var postMeta = snapshot.val();
 
+                console.log("postMeta.timestamp: " + postMeta.timestamp + "real date : " + new Date(postMeta.timestamp));
+                console.log("postMeta.ttl: " + postMeta.ttl);
 
-            console.log("postMeta.timestamp: " + postMeta.timestamp + "real date : " + new Date(postMeta.timestamp));
-            console.log("postMeta.ttl: " + postMeta.ttl);
+                if (postMeta.timestamp + postMeta.ttl * HOUR_IN_MILLI_SECONDS < Date.now()) {
+                    console.log("postId: " + snapshot.key + " should be deleted");
+                    snapshot.ref.child("delete").set(true);
+                } else {
+                    console.log("postId: " + snapshot.key + " should  not be deleted");
 
-            if(postMeta.timestamp + postMeta.ttl*HOUR_IN_MILLI_SECONDS  < Date.now()){
-                console.log("postId: "+snapshot.key+" should be deleted");
-                snapshot.ref.child("delete").set(true);
-            }
-            else{
-                console.log("postId: "+snapshot.key+" should  not be deleted");
-
-            }
-        });
-            resolve("start schedule for deletion messages")
+                }
+            });
+            resolve()
         });
     });
     return promise;
 };
-
+/**
+ *  Schedulers for mark last day chat messages as "should be deleted"
+ * @returns {*|Promise}
+ */
 var scheduleDeletionMessages = function() {
-    var promise = new Promise(function(resolve, reject){
+    var promise = new Promise(function(resolve, reject) {
         var db = firebase.database();
         var ref = db.ref("/chats/messages-meta/").orderByChild("delete").equalTo(false);
 
-        cron.schedule('* * * * *', function(){
+        cron.schedule('0 * * * *', function() {
             console.log('running a schedule every hour');
 
             ref.on("child_added", function(snapshot, prevChildKey) {
@@ -160,12 +140,11 @@ var scheduleDeletionMessages = function() {
                 console.log("messageMeta.timestamp: " + messageMeta.timestamp + "real date : " + new Date(messageMeta.timestamp));
                 console.log("messageMeta.ttl: " + messageMeta.ttl);
 
-                if(messageMeta.timestamp + messageMeta.ttl*HOUR_IN_MILLI_SECONDS  < Date.now()){
-                    console.log("messageId: "+snapshot.key+" should be deleted");
+                if (messageMeta.timestamp + messageMeta.ttl * HOUR_IN_MILLI_SECONDS < Date.now()) {
+                    console.log("messageId: " + snapshot.key + " should be deleted");
                     snapshot.ref.child("delete").set(true);
-                }
-                else{
-                    console.log("messagetId: "+snapshot.key+" should  not be deleted");
+                } else {
+                    console.log("messagetId: " + snapshot.key + " should  not be deleted");
 
                 }
             });
@@ -174,27 +153,26 @@ var scheduleDeletionMessages = function() {
     return promise;
 };
 
-var triggerUpload = function (){
-    upload_resources_service.uploadResources(firebase,bucket)
+var triggerUpload = function() {
+    upload_resources_service.uploadResources(firebase, bucket)
 }
+//in app provisioning set the 4th argument to true (upload images)
 var loadResources = false;
-if(process.argv[4]!=null){
+if (process.argv[4] != null) {
     loadResources = (process.argv[4] === 'true');
 }
-if(loadResources){
+if (loadResources) {
     buildServiceAccountPath()
-    .then(loadServiceAccountPath)
-    .then(triggerUpload);
+        .then(loadServiceAccountFromPath)
+        .then(triggerUpload);
 }
+
 buildServiceAccountPath()
-    .then(loadServiceAccountPath)
+    .then(loadServiceAccountFromPath)
     .then(scheduleDeletionPosts);
 
 buildServiceAccountPath()
-    .then(loadServiceAccountPath)
+    .then(loadServiceAccountFromPath)
     .then(scheduleDeletionMessages);
-   // .then(uploadProfileImagesThumb);
 
-    //.then(uploadProfileImagesThumb);
 return;
-
